dd1001f3e9a6b2807392c27d2cd237caacc6a014
Yeo Wen Qin, Kaynn <KaynnCahya@outlook.com>
2023-03-07 12:58:09 +0800

    Refactor; Added a search bar

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
--->
 <html>
-    <link rel="stylesheet" href="css/shopping_list.css">
+    <link rel="stylesheet" href="css/book_listing.css">
     <link rel="stylesheet"
         href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity=
@@ -34,40 +30,8 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
             include "nav.inc.php";
         ?>
         
-        <div class="container">
-          <h1 class="text-center my-4">Featured Books</h1>
-          <div id="book-list" class="row"></div>
-        </div>
-
-        <!--The list of books-->
-        <div class="modal fade" id="book-modal" tabindex="-1" role="dialog" aria-labelledby="book-modal-label" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="book-modal-label"></h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                <div class="row">
-                  <div class="col-sm-4">
-                    <img id="book-modal-image" class="img-fluid" src="images/not_found.png">
-                  </div>
-                  <div class="col-sm-8">
-                    <p id="book-modal-description"></p>
-                    <p id="book-modal-price"></p>
-                  </div>
-                </div>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Add to Cart</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        
-        <script src="js/shopping_list.js"></script>
+        <?php
+            include "book_list_php/book_listing.php";
+        ?>
     </body>
 </html>
