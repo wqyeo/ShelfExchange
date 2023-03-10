2252bcc41632b024fea2fd3c0523a301236be0b6
Jonathan Chiu <101392243+JczhStudy@users.noreply.github.com>
2023-03-10 10:46:57 +0800

    Edited

--- adminPage.php
+++ adminPage.php
@@ -15,21 +15,24 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
               href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
               integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
               crossorigin="anonymous">
-        <link rel="stylesheet" href="css/adminPage.css">
+        
          <!--jQuery-->
         <script defer
                 src="https://code.jquery.com/jquery-3.4.1.min.js"
                 integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
                 crossorigin="anonymous">
         </script>
+        
         <!--Bootstrap JS-->
         <script defer
                 src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"
                 integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm"
                 crossorigin="anonymous">
         </script>
-        <!-- Custom JS -->
+        
+        <!-- Custom JS & CSS -->
         <script defer src="js/main.js"></script>
+        <link rel="stylesheet" href="css/adminPage.css">
     </head>
     
     <body>
