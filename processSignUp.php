b20cfd5e499b386f0a6f8b03c818d749f109e9bc
Jr-Goh <2202074@sit.singaporetech.edu.sg>
2023-03-12 16:56:57 +0800

    For signup & login
    WIP signup and login

---
+++ processSignUp.php
@@ -0,0 +1,116 @@
+
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"crossorigin="anonymous">
+        <!--jQuery-->
+        <script defer src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <!--Bootstrap JS-->
+        <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>
+        <!-- Custom JS -->
+        <script defer src="js/main.js"></script>
+        <link rel="stylesheet" href="css/main.css">
+        <title>ShelfExchange</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <?php
+        $username = $contactNo = $pwd = $pwd_confirm = $email = $errorMsg = "";
+        $success = true;
+        
+        if (empty($_POST["username"])) {                   //If the form value of lname is not empty, then it will sanitize the input
+            $errorMsg .= "Username is required.<br>";
+            $success = false;
+        } else {
+            $username = sanitize_input($_POST["username"]);
+        }
+        if (empty($_POST["contactNo"])) {
+            $errorMsg .= "Contact Number is required.<br>";
+            $success = false;
+        } else{
+            $contactNo = sanitize_input($_POST["contactNo"]);
+        }
+        if (empty($_POST["pwd"])) {                     //If the form value of pwd is not empty, then it will sanitize the input
+            $errorMsg .= "Password is required.<br>";
+            $success = false;
+        } else {
+            //Do hashing
+            $pwd = $_POST['pwd'];
+            $hashed_password = password_hash($pwd, PASSWORD_DEFAULT);
+            //var_dump($hashed_password); //This is to check if the password is hashed by displaying it out.
+        }
+        if (empty($_POST["pwd_confirm"])) {            //If the form value of pwd_confirm is not empty, then it will sanitize the input
+            $errorMsg .= "Password confirmation required.<br>";
+            $success = false;
+        } elseif ($_POST["pwd"] === $_POST["pwd_confirm"]) {
+        } else {
+            $errorMsg .= "Password and confirm password are not the same!";
+            $success = false;
+        }
+
+        if (empty($_POST["email"])) {
+            $errorMsg .= "Email is required.<br>";
+            $success = false;
+        } else {
+            $email = sanitize_input($_POST["email"]);
+            // Additional check to make sure e-mail address is well-formed.
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $errorMsg .= "Invalid email format.";
+                $success = false;
+            }
+        }
+        if ($success) {
+            saveMemberToDB();
+            echo "<h4>Registration successful!</h4>";
+            echo "<p>Email: " . $email;
+            echo '<form name="return" id="return" action="/Lab09/register.php">';
+            echo '<button class="btn btn-primary" type="submit">Return</button>';
+            echo '</form>';
+        } else {
+            echo "<h4>The following input errors were detected:</h4>";
+            echo "<p>" . $errorMsg . "</p>";
+            echo '<form name="return" id="return" action="/Lab09/register.php">';
+            echo '<button class="btn btn-danger" type="submit">Return</button>';
+            echo '</form>';
+        }
+
+        //Helper function that checks input for malicious or unwanted content.
+        function sanitize_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+        
+        //Helper function to write the member data to the DB
+        function saveMemberToDB() {
+            global $username, $email, $hashed_password, $errorMsg, $success;   //This is to access the global variables.
+// Create database connection.
+            $config = parse_ini_file('../../private/db-config.ini');
+            $conn = new mysqli($config['servername'], $config['username'],
+                    $config['password'], $config['dbname']);
+// Check connection
+            if ($conn->connect_error) {
+                $errorMsg = "Connection failed: " . $conn->connect_error;
+                $success = false;
+            } else {
+                echo "Connected Successfully!"; // For testing connection
+// Prepare the statement:
+                $stmt = $conn->prepare("INSERT INTO user (username, email, password) VALUES (?,?,?)");
+// Bind & execute the query statement:
+                $stmt->bind_param("sss", $username, $email, $hashed_password);
+                if (!$stmt->execute()) {
+                    $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+                    $success = false;
+                }
+                $stmt->close();
+            }
+            $conn->close();
+        }
+        
+        ?>
+        
+        <main>
+        </main>
+    </body>
+</html>
---
+++ signUp.php
@@ -0,0 +1,53 @@
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"crossorigin="anonymous">
+        <!--jQuery-->
+        <script defer src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <!--Bootstrap JS-->
+        <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>
+        <!-- Custom JS -->
+        <script defer src="js/main.js"></script>
+        <link rel="stylesheet" href="css/main.css">
+        <title>ShelfExchange</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <main class="container">
+            <h1>User Registration</h1>
+            <p>
+                For existing users, please go to the
+                <a href="login.php">Login page</a>.
+            </p>
+            <form action="processSignUp.php" method="post">
+                <div class="form-group">
+                <label for="username">Username:</label> <!-- required -->
+                <input class="form-control" type="text" id="lname" required maxlength="45" name="lname" placeholder="Enter last name">
+                </div>
+                
+                <div class="form-group">
+                    <labe for="contactNo">Contact Number:</label>
+                    <input class="form-control" type="text" id="contactNo" required maxlength="45" name="contactNo" placeholder="Enter contact number">
+                </div>
+                
+                <div class="form-group">
+                <label for="email">Email:</label> <!-- required -->
+                <input class="form-control" type="email" id="email" required maxlength="255" name="email" placeholder="Enter email">
+                </div>
+                
+                <div class="form-group">
+                <label for="pwd">Password:</label> <!-- required -->
+                <input class="form-control" type="password" id="pwd" required maxlength="255" name="pwd" placeholder="Enter password">
+                </div>
+                
+                <div class="form-group"> <!-- required -->
+                <label for="pwd_confirm">Confirm Password:</label>
+                <input class="form-control" type="password" id="pwd_confirm" required maxlength="255" name="pwd_confirm" placeholder="Confirm password">
+                </div>
+                
+                <br>
+                <button class="btn btn-primary" type="submit">Sign up</button>
+            </form>
+        </main>
+    </body>
+</html>
