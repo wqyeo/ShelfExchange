57a583cb063c552a982b1961d7c0e2c62d9baa8f
Yeo Wen Qin, Kaynn <KaynnCahya@outlook.com>
2023-03-14 07:58:20 +0800

    Refactor code on SignUp process database statements

--- src/processSignUp.php
+++ src/processSignUp.php
@@ -1,98 +1,100 @@
-        <?php
-        $username = $pwd = $pwd_confirm = $email = $errorMsg = "";
-        $success = true;
+<?php
+    // Check if form was submitted
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  
+        // Check if all required fields are present and not empty
+        if (empty($_POST["username"]) || empty($_POST["email"]) || empty($_POST["password"])) {
+            header("Location: signUp.php?error=missing_fields");
+            exit();
+        }
+
+        // Sanitize and validate the username
+        $usernameValidated = tryGetValidatedUsername();
+        $emailValidated = tryGetValidatedEmail();
+        $passwordValidated = tryGetValidatedPassword();
+
         
-        if (empty($_POST["username"])) {                   
-            $errorMsg .= "Username is required.<br>";
-            $success = false;
-        } else {
-            $username = sanitize_input($_POST["username"]);
+        saveNewUser($usernameValidated, $passwordValidated, $emailValidated);
+
+        header("Location: login.php?success=signup_complete");
+        exit();
+    } else {
+        // Form was not submitted, redirect to the signup page
+        header("Location: signUp.php");
+        exit();
+    }
+    
+    function tryGetValidatedUsername(){
+        $username = filter_var(trim($_POST["username"]), FILTER_SANITIZE_STRING);
+        if (strlen($username) < 4) {
+            header("Location: signUp.php?error=username_short");
+            exit();
+        }
+        if (strlen($username) > 45) {
+            header("Location: signUp.php?error=username_long");
+            exit();
+        }
+        return $username;
+    }
+    
+    function tryGetValidatedEmail(){
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            header("Location: signUp.php?error=email_invalid");
+            exit();
+        }
+        return $email;
+    }
+    
+    function tryGetValidatedPassword(){
+        $password = $_POST["password"];
+        if (strlen($password) < 8) {
+            header("Location: signUp.php?error=password_short");
+            exit();
+        }
+        if (strlen($password) > 64) {
+            header("Location: signUp.php?error=password_long");
+            exit();
+        }
+        return $password;
+    }
+    
+    function saveNewUser($newUsername, $newPassword, $newEmail){
+        $connection = createDatabaseConnection();
+        if ($connection->connect_error) {
+            header("Location: signUp.php?error=connection_failed($connection->connect_error)");
+            exit();
         }
         
-        if (empty($_POST["pwd"])) {                     
-            $errorMsg .= "Password is required.<br>";
-            $success = false;
-        } else {
-            $pwd = $_POST['pwd'];
-            $hashed_password = password_hash($pwd, PASSWORD_DEFAULT);
+        $todayDate = getCurrentDate();
+        $statement = prepareBindedInsertUserStatement($connection, $newEmail, $newUsername, $newPassword, $todayDate);
+        if (!$statement->execute()) {
+            // TODO: Error message should be recorded into a log file that can be readed from server.
+            //$errorMsg = "Execute failed: (" . $statement->errno . ") " . $statement->error;
+            header("Location: signUp.php?error=connection_failed(FATAL)");
+            exit();
         }
-        if (empty($_POST["pwd_confirm"])) {            //If the form value of pwd_confirm is not empty, then it will sanitize the input
-            $errorMsg .= "Password confirmation required.<br>";
-            $success = false;
-        } elseif ($_POST["pwd"] === $_POST["pwd_confirm"]) {
-        } else {
-            $errorMsg .= "Password and confirm password are not the same!";
-            $success = false;
-        }
-
-        if (empty($_POST["email"])) {
-            $errorMsg .= "Email is required.<br>";
-            $success = false;
-        } else {
-            $email = sanitize_input($_POST["email"]);
-            // Additional check to make sure e-mail address is well-formed.
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $errorMsg .= "Invalid email format.";
-                $success = false;
-            }
-        }
-        if ($success) {
-            saveMemberToDB();
-            echo "<h4>Registration successful!</h4>";
-            echo "<p>Email: " . $email;
-            echo "<p>Username: " . $username;
-            echo '<form name="return" id="return" action="/ShelfExchange/signUp.php">';
-            echo '<button class="btn btn-primary" type="submit">Return</button>';
-            echo '</form>';
-        } else {
-            echo "<h4>The following input errors were detected:</h4>";
-            echo "<p>" . $errorMsg . "</p>";
-            echo '<form name="return" id="return" action="/ShelfExchange/signUp.php">';
-            echo '<button class="btn btn-danger" type="submit">Return</button>';
-            echo '</form>';
-        }
-
-        //Helper function that checks input for malicious or unwanted content.
-        function sanitize_input($data) {
-            $data = trim($data);
-            $data = stripslashes($data);
-            $data = htmlspecialchars($data);
-            return $data;
-        }
-
-        function null_or_empty($fieldName){
-            return !isset($_POST[$fieldName]) || empty($_POST[$fieldName]);
-        }
-        
-        //Helper function to write the member data to the DB
-        function saveMemberToDB() {
-            global $todayDate, $contactNo, $username, $email, $hashed_password, $errorMsg, $success;   //This is to access the global variables.
-            
-            $todayDate = date("Y/m/d");
-// Create database connection.
-
-            $config = parse_ini_file('../../private/db-config.ini');
-            $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
-            
-// Check connection
-            if ($conn->connect_error) {
-                $errorMsg = "Connection failed: " . $conn->connect_error;
-                $success = false;
-            } else {
-                echo "Connected Successfully!"; // For testing connection
-// Prepare the statement:
-                $stmt = $conn->prepare("INSERT INTO user (email, username, password, joined_date) VALUES (?, ?, ?, ?)");
-// Bind & execute the query statement:
-                $stmt->bind_param("sssss", $email, $username, $hashed_password, $todayDate);
-                if (!$stmt->execute()) {
-                    echo "GG. Failed";
-                    $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-                    $success = false;
-                    echo "GG. Failed2";
-                }
-                $stmt->close();
-            }
-            $conn->close();
-        }
+        $statement->close();
+        $connection->close();
+    }
+    
+    // Prepares a SQL statement to insert new user, and binds the given input
+    // NOTE: It this function will input password.
+    // Returns, the statement.
+    function prepareBindedInsertUserStatement($connection, $email, $username, $rawPassword, $joinedDate) {
+        $stmt = $connection->prepare("INSERT INTO user (email, username, password, joined_date) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $email, $username, password_hash($rawPassword, PASSWORD_DEFAULT), $joinedDate);
+        return $stmt;
+    }
+    
+    function getCurrentDate() {
+        date_default_timezone_set('Asia/Singapore');
+        return date('Y-m-d');
+    }
+    
+    function createDatabaseConnection() {
+        $config = parse_ini_file('../../private/db-config.ini');
+        return new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
+    }
         
 ?>
