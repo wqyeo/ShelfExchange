c5229a9d786ee58d998232cdb480651d3bc59af8
zininngg <ziningtan12@gmail.com>
2023-03-15 17:05:38 +0800

    create aboutUs

---
+++ src/aboutUs.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Shop Homepage - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <?php
+            include "nav.php"
+        ?>
+        <div id="abtus">
+            <h1 class="display-4 fw-bolder">BRAND PROFILE</h1>
+            
+            <p class="lead fw-normal text-white-50 mb-0">
+                At Shelf Exchange we offer a wide variety of books for all different types of bookworms. Whether you're looking for the latest mystery novel, a classic romance, or a how-to guide, we've got you covered.
+
+                We understand that everyone has different taste in literature, which is why we offer such a diverse selection of books. Whether you're looking to be scared, laugh, or cry, we have a book that will fit your needs.
+
+                If you're looking for something new to read, but don't know where to start, our team of experts are always on hand to recommend the perfect book for you.
+            </p>
+            
+        </div>
+        
+        <?php
+            include "footer.php"
+        ?>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
