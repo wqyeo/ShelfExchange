d324c2c43d58901c5bb0c0c1e3d104499124bb1e
Jr-Goh <2202074@sit.singaporetech.edu.sg>
2023-03-17 10:28:18 +0800

    Changes to login.php

--- src/login.php
+++ src/login.php
@@ -8,34 +8,45 @@
         <!-- Custom JS -->
         <script defer src="js/main.js"></script>
         <link rel="stylesheet" href="css/main.css">
-        <title>ShelfExchange</title>
+        <title>Shelf Exchange Login</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="css/login.css" rel="stylesheet" />
     </head>
     <body>
         <?php
         include "nav.php";
         ?>
-        <main class="container">
-            <h1>User Login</h1>
-            <p>
-                For existing users, please go to the
-                <a href="signUp.php">Sign Up page</a>.
-            </p>
-            <form action="processLogin.php" method="post">
-                
-                <div class="form-group">
-                <label for="email">Email:</label> <!-- required -->
-                <input class="form-control" type="email" id="email" required maxlength="255" name="email" placeholder="Enter email">
+        <!-- Big header asking for user to sign up-->
+        <header class="bg-dark py-2">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">Login</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">Get more offers and benefits when browsing!</p>
+                </div>
+            </div>
+        </header>
+        <main class="container mt-3">
+            <form action="processLogin.php" method="post">   
+                <div class="mb-3">
+                  <label for="email" class="form-label">Email</label>
+                  <input type="email" class="form-control" id="email" name="email" required>
+                  <div id="emailError" class="text-danger"></div>
                 </div>
                 
-                <div class="form-group">
-                <label for="pwd">Password:</label> <!-- required -->
-                <input class="form-control" type="password" id="pwd" required maxlength="255" name="pwd" placeholder="Enter password">
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
                 </div>
                 
-                <br>
-                <button class="btn btn-primary" type="submit">Login</button>
+                <button type="submit" class="btn btn-primary mb-3">Log in</button>
+                <p>Already have an account? <a href="signUp.php">Sign up</a></p>
             </form>
         </main>
         <?php
