922f803aedf864d68faeef3e29e246e46254d314
Yeo Wen Qin, Kaynn <KaynnCahya@outlook.com>
2023-03-26 14:06:07 +0800

    Display server-side error to user during signup

--- src/signUp.php
+++ src/signUp.php
@@ -35,19 +35,46 @@
                 </div>
             </div>
         </header>
+
+        <!--Find and display server side error to user.-->
+    <?php
+        require 'php_error_models/signUpErrorCode.php';
+        $errorMessage = $_GET['error'];
+
+        $emailErrorMessage = "";
+        $usernameErrorMessage = "";
+        $genericErrorMessage = "";
+        if (isset($errorMessage)) {
+            if ($errorMessage == SignUpErrorCode::EMAIL_USED) {
+                $emailErrorMessage = "The email is already in use!";
+            } elseif ($errorMessage == SignUpErrorCode::USERNAME_USED) {
+                $usernameErrorMessage = "Sorry, the username is already taken!";
+            } elseif ($errorMessage == SignUpErrorCode::CONNECTION_FAILED_STATEMENT_ERROR || $errorMessage == SignUpErrorCode::CONNECTION_FAILED) {
+                $genericErrorMessage = "Error trying to sign up, contact support!";
+            } elseif ($errorMessage == SignUpErrorCode::MISSING_FIELDS) {
+                $genericErrorMessage = "Ensure all fields are filled!";
+            } elseif ($errorMessage == SignUpErrorCode::EMAIL_INPUT_INVALID) {
+                $emailErrorMessage = "The email doesn't seem correct.";
+            } elseif ($errorMessage == SignUpErrorCode::USERNAME_INPUT_INVALID) {
+                $usernameErrorMessage = "Username too long or short.";
+            } elseif ($errorMessage == SignUpErrorCode::PASSWORD_INPUT_INVALID) {
+                $genericErrorMessage = "Password too short or long";
+            }
+        }
+    ?>
         
         <main class="container mt-3">
             <form action="processSignUp.php" method="post" onsubmit="return validateForm()">
                 <div class="mb-3">
                   <label for="username" class="form-label">Username</label>
                   <input type="text" class="form-control" id="username" name="username" required>
-                  <div id="usernameError" class="text-danger"></div>
+                  <div id="usernameError" class="text-danger"><?php echo $usernameErrorMessage; ?></div>
                </div>
                 
                 <div class="mb-3">
                   <label for="email" class="form-label">Email</label>
                   <input type="email" class="form-control" id="email" name="email" required>
-                  <div id="emailError" class="text-danger"></div>
+        <div id="emailError" class="text-danger"><?php echo $emailErrorMessage; ?></div>
                 </div>
                 
                 <div class="mb-3">
@@ -58,6 +85,11 @@
                         <div id="password-strength-bar" class="progress-bar" role="progressbar"></div>
                     </div>
                     <div id="passwordError" class="text-danger"></div>
+                </div>
+
+                <div class="mb-1">
+                    <!--For any generic error in form submission-->
+                    <div id="genericError" class="text-danger"><?php echo $genericErrorMessage; ?></div>
                 </div>
                 
                 <button type="submit" class="btn btn-primary mb-3">Sign up</button>
