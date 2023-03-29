1b7f2b7fa89ca081ea356fa0a6e4052f3850a6e3
Evan <118990309+EvanChua22@users.noreply.github.com>
2023-03-29 17:55:33 +0800

    Update finally works
    Going to work on editprofile

---
+++ src/js/updateOrDelUserAcc.js
@@ -0,0 +1,44 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+function getUserDetails(updateID) {
+    $('#UserId').val(updateID);
+
+    $.post("updateUser.php", {updateID: updateID}, function (data, status) {
+        var userid = JSON.parse(data);
+        $('#updateUsername').val(userid.username);
+        $('#updateEmail').val(userid.email);
+    });
+    $('#updateUserDetails').modal("show");
+}
+
+function updateDetails() {
+    var updateUsername = $('#updateUsername').val();
+    var updateEmail = $('#updateEmail').val();
+    var UserId = $('#UserId').val();
+
+    $.post("updateUser.php", {
+        updateUsername: updateUsername,
+        updateEmail: updateEmail,
+        UserId: UserId
+    }, function (data, status) {
+        $('#updateUserDetails').modal('hide');
+        location.href = 'testAPAcc.php';
+    });
+}
+
+function deleteUser(deleteID) {
+    $.ajax({
+        url: "delete.php",
+        type: 'post',
+        data: {
+            deleteid: deleteID
+        },
+        success: function (data, status) {
+            location.href = 'testAPAcc.php';
+
+        }
+    });
+}
+
