124233fa62131102bc440de9201ba2c2ead4c4ef
Yeo Wen Qin, Kaynn <KaynnCahya@outlook.com>
2023-03-31 19:56:32 +0800

    Tryfix bug with user sometimes not being deleted; Also prevent SQL injec

--- src/deleteUser.php
+++ src/deleteUser.php
@@ -1,16 +1,23 @@
 <?php
 
-include_once "php_util/util.php";
-$connection = createDatabaseConnection();
-
-// Check connection
-if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
 if (isset($_POST['deleteid'])) {
+    include_once "php_util/util.php";
+    $connection = createDatabaseConnection();
+
+    // Check connection
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
     $unique = $_POST['deleteid'];
 
-    $sql="DELETE FROM user WHERE id=$unique";
-    $result = mysqli_query($connection, $sql);
+    $sql="DELETE FROM user WHERE id=?";
+    $statement = $connection->prepare($sql);
+    $statement->bind_param("i", $unique);
+
+    if ($statement->execute()) {
+        //success
+    }
+    $statement->close();
+    $connection->close();
 }
